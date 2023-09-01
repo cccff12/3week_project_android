eaa155059bccc2bc81344bf8e107974988759c14
cccff12 <ksun0430@naver.com>
2023-09-01 15:57:25 +0900

    스플래시적용

--- cook/lib/main.dart
+++ cook/lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -23,6 +26,30 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("앱 바 타이틀")),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 192, 203, 1),
+              Color.fromRGBO(243, 243, 131, 1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: const Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "레시피 제목을 입력해주세요",
+                  labelStyle: TextStyle(fontSize: 20, color: Colors.amber),
+                  hintText: "아잉"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
