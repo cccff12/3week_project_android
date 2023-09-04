3c63f766d0c2487373d2f8014d8fb67e57ae0053
cccff12 <ksun0430@naver.com>
2023-09-04 22:05:46 +0900

    저장

--- cook/lib/main.dart
+++ cook/lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: SttPage());
+      debugShowCheckedModeBanner: false,
+      home: SttPage(),
+    );
   }
 }
 
